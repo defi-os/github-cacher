d887a4d8b7fb0795701235029ae8248744ca1141
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-07-10 20:01:20 +0530

    added code for cronjob.py

--- github_connector/github.py
+++ github_connector/github.py
@@ -26,14 +26,20 @@ class GithubConnector:
         )
         data = response.json()
 
-        if response.status_code != 200 or "items" not in data:
+        if response.status_code != 200:
+            return None
+        if "items" not in data:
             return []
-        return data["items"]
+        repos = [
+            {"id": repo["id"], "languages_url": repo["languages_url"]}
+            for repo in data["items"]
+        ]
+        return repos
 
     @staticmethod
-    def calculate_rust_percentage(languages_data):
+    def calculate_langauge_percentage(langauge, languages_data):
         total_bytes = sum(languages_data.values())
-        rust_bytes = languages_data.get("Rust", 0)
+        rust_bytes = languages_data.get(langauge, 0)
 
         if total_bytes > 0:
             return rust_bytes / total_bytes
@@ -41,17 +47,44 @@ class GithubConnector:
             return 0.0
 
     @staticmethod
-    def filter_rust_repositories(repositories, threshold):
+    def filter_repositories(langauge, repositories, threshold):
         filtered_repos = []
         for repo in repositories:
-            repo_url = repo["html_url"]
-            repo_name = repo["name"]
+            repo_id = repo["id"]
             languages_url = repo["languages_url"]
-
-            response = requests.get(languages_url)
-            languages_data = response.json()
-            rust_percentage = GithubConnector.calculate_rust_percentage(languages_data)
+            try:
+                response = requests.get(languages_url)
+                languages_data = response.json()
+                rust_percentage = GithubConnector.calculate_langauge_percentage(
+                    langauge, languages_data
+                )
+            except:
+                continue
             if rust_percentage >= threshold:
-                filtered_repos.append((repo_name, repo_url))
+                filtered_repos.append(repo_id)
 
         return filtered_repos
+
+    def get_issues_by_repo(self, repo_id):
+        url = f"{self.url}/repositories/{repo_id}/issues"
+        headers = {
+            "Accept": "application/vnd.github.mercy-preview+json",
+            "X-GitHub-Api-Version": "2022-11-28",
+            "Authorization": f"Bearer {self.__access_token}",
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            issues = response.json()
+            issues = [
+                {
+                    "title": issue["title"],
+                    "description": issue["body"],
+                    "number": issue["url"].split("issues/")[1],
+                }
+                for issue in issues
+            ]
+            return issues
+        else:
+            return None
